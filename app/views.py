8c62868c1a852d0429f140d90543a0ffe8ccd19d
Pedro Rufino Segovia <pedro.rufino.segovia@gmail.com>
2019-03-18 18:57:06 +0100

    nuevacompra

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route("/")
 def index():
-    return render_template("inderx.html")
+    return render_template("index.html")
+
+@app.route('/nuevacompra')
+def  nuevacompra():
+    return render_template("nuevacompra.html")
+
+
--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hola, Mundo"
+
+@app.route('/registro')
+def comenzamos():
+    return "Ahora ya exsisto"
