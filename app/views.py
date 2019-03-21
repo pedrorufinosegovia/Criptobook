aca10458da50c9f2cfd388f4a246646416fc3c23
Pedro Rufino Segovia <pedro.rufino.segovia@gmail.com>
2019-03-21 21:12:24 +0100

    masavances

--- app/views.py
+++ app/views.py
@@ -24,7 +24,20 @@ def index():
 @app.route('/nuevacompra', methods=["GET", "POST"])
 def  nuevacompra():
     if request.method == "GET":
-        return render_template("nuevacompra.html")
+        if request.values['btnselected'] == 'Nueva':
+            return render_template("nuevacompra.html")
+        else:
+            ix = request.values['ix']
+            ix = float(ix)
+            transacciones = open(ficherotransacciones, "r")
+            csvreader = csv.reader(transacciones, delimiter=",", quotechar='"')
+            for numreg, registro in enumerate(csvreader):
+                if numreg == ix:
+                    camposdict ={}
+                    for ix, field in enumerate(fields):
+                        camposdict[field] = registro[ix]
+                    return render_template("modicacompra.html", registro = camposdict)
+            return "movimiento no encontrado"
     else:
         transacciones = open(ficherotransacciones, "+a")
         nada = "{},{},'{}',{},{},{},{}\n".format(request.form["fecha"],request.form["hora"],request.form["descripcion"],request.form["monedacomprada"],request.form["cantidadcomprada"],request.form["monedapagada"],request.form["cantidadpagada"])
